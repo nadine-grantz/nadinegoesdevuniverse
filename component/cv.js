0aefb613d2a01a3b865b68e429ee21af2f857bb9
Nadine Grantz <nadine.grantz@googlemail.com>
2024-05-07 14:58:20 +0200

    ad function for cv

---
+++ component/cv.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function CV() {
+  return (
+    <>
+      <p>hier der cv</p>
+    </>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import css from "styled-jsx/css";
+import CV from "../component/cv";
+
+export default function Home() {
+  return (
+    <main>
+      <CV />
+    </main>
+  );
+}
