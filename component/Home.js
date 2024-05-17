4cb793d292da25a62c48b605e8131cd4f3478191
Nadine Grantz <nadine.grantz@googlemail.com>
2024-05-17 11:54:23 +0200

    add Linkedin Link

--- component/Home.js
+++ component/Home.js
@@ -29,6 +29,9 @@ export default function Home() {
         As you can see, the website is not ready, still working on it
       </StyledText>
       <StyledLink href="https://github.com/nadine-grantz">github</StyledLink>
+      <StyledLink href="https://www.linkedin.com/in/nadine-grantz-36a03a1ba/">
+        Linkedin
+      </StyledLink>
     </SectionContainer>
   );
 }
