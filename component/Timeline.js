734218038bcaf463d4e27f9b4ea8dd39f30029b6
Nadine Grantz <nadine.grantz@googlemail.com>
2024-05-22 22:18:08 +0200

    change styling with styled component

--- component/Timeline.js
+++ component/Timeline.js
@@ -1,59 +1,83 @@
 import React from "react";
+import styled from "styled-components";
 import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 
+const Section = styled.section`
+  #cv {
+    padding: 20px;
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 2.25rem; /* entspricht text-4xl */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 2rem; /* entspricht mt-8 */
+`;
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+`;
+
+const StyledDate = styled.div`
+  margin: 0 1rem; /* entspricht mx-4 */
+`;
+
+const timelineItems = [
+  {
+    date: "Januar 2011 - Oct. 2021",
+    title: "Jobtitle",
+    location: "Hamburg",
+  },
+  {
+    date: "Januar 2011 - Oct. 2021",
+    title: "Jobtitle",
+    location: "Hamburg",
+  },
+  {
+    date: "Januar 2011 - Oct. 2021",
+    title: "Jobtitle",
+    location: "Hamburg",
+  },
+  {
+    date: "Januar 2011 - Oct. 2021",
+    title: "Jobtitle",
+    location: "Hamburg",
+    content: "Hier der Content",
+  },
+];
+
+const icons = [
+  <img
+    src="https://www.santander.de/ressourcen/img/svg/santander-logo-2018.svg"
+    alt="Santander Logo"
+  />,
+  <img
+    src="https://www.targobank.de/de/amc-content/svg/assets/icon-tab.svg"
+    alt="Targobank Logo"
+  />,
+];
+
+const color = "#00ffe2";
+
 export default function Timeline() {
-  const timelineItems = [
-    {
-      date: "Januar 2011 - Oct. 2021",
-      title: "Jobtitle",
-      location: "Hamburg",
-    },
-    {
-      date: "Januar 2011 - Oct. 2021",
-      title: "Jobtitle",
-      location: "Hamburg",
-    },
-    {
-      date: "Januar 2011 - Oct. 2021",
-      title: "Jobtitle",
-      location: "Hamburg",
-    },
-    {
-      date: "Januar 2011 - Oct. 2021",
-      title: "Jobtitle",
-      location: "Hamburg",
-      content: "Hier der Content",
-    },
-  ];
-
-  const icons = [
-    <img
-      src="https://www.santander.de/ressourcen/img/svg/santander-logo-2018.svg"
-      alt="Santander Logo"
-    />,
-    <img
-      src="https://www.targobank.de/de/amc-content/svg/assets/icon-tab.svg"
-      alt="Targobank Logo"
-    />,
-  ];
-
-  const color = "#00ffe2";
-
   return (
-    <section id="cv">
-      <h1 class="text-4xl flex items-center justify-center mt-8">Timeline</h1>
-      <div className="mx-auto">
+    <Section id="cv">
+      <Title>Timeline</Title>
+      <Container>
         <VerticalTimeline>
           {timelineItems.map((item, index) => (
             <VerticalTimelineElement
               key={index}
-              date={<div className="mx-4">{item.date}</div>}
+              date={<StyledDate>{item.date}</StyledDate>}
               contentStyle={{
-                background: index % 2 == 0 ? "#00ffe2" : "white",
+                background: index % 2 === 0 ? color : "white",
                 color: "black",
               }}
               contentArrowStyle={{ borderRight: "7px solid #3498db" }}
@@ -64,7 +88,7 @@ export default function Timeline() {
                 padding: "16px",
                 marginRight: "4px",
               }}
-              icon={icons[index]}
+              icon={icons[index % icons.length]}
             >
               <h3 className="vertical-timeline-element-title">{item.title}</h3>
               <h4 className="vertical-timeline-element-subtitle">
@@ -74,7 +98,7 @@ export default function Timeline() {
             </VerticalTimelineElement>
           ))}
         </VerticalTimeline>
-      </div>
-    </section>
+      </Container>
+    </Section>
   );
 }
