98132559ab7c6fdbf06e93ac9b614c4cfe2ebf13
Nadine Grantz <nadine.grantz@googlemail.com>
2024-05-27 11:36:15 +0200

    add text for plant World

--- component/Projects.js
+++ component/Projects.js
@@ -16,6 +16,19 @@ export default function Home() {
   return (
     <SectionContainer id="projects">
       <StyledHeader>Projects</StyledHeader>
+      <img
+        src="/home/danielbechler/Pictures/Bildschirmfotos/Bildschirmfoto%20von%202024-04-30%2015.50.34_ver%C3%A4ndert.png"
+        alt="plant-World App Picture"
+      />
+      <div>
+        Die App ist der ideale Begleiter für neue und erfahrene
+        Pflanzenliebhaber. Sie bietet Pflegetipps für viele Pflanze in deiner
+        Sammlung. Benutzer können ihre eigenen Pflanzen hinzufügen. Eine
+        intuitive Benutzeroberfläche macht es einfach, den Überblick über die
+        Pflanzenpflege zu behalten. Mit dieser App wird Ihr Zuhause zum grünen
+        Paradies! Die App ist noch im Aufbau, schließlich gibt es immer etwas zu
+        verbessern.
+      </div>
     </SectionContainer>
   );
 }
