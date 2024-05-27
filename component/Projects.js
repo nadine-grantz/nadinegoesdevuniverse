7cc4eda729a58fc0290ae30634a6ec64c2d170f3
Nadine Grantz <nadine.grantz@googlemail.com>
2024-05-27 18:30:42 +0200

    change styling of project container

--- component/Projects.js
+++ component/Projects.js
@@ -36,12 +36,12 @@ const ProjectContainer = styled.div`
 `;
 
 const ProjectTitle = styled.h3`
-  position: absolute;
+  position: left;
   top: 1rem;
   right: 1rem;
   margin: 0;
   font-size: 1.2rem;
-  color: #333;
+  color: black;
 `;
 
 const ContentWrapper = styled.div`
@@ -58,13 +58,18 @@ const StyledImage = styled.img`
   margin-right: 1rem;
 `;
 
+const DescriptionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 const ProjectDescription = styled.div`
   max-width: 15rem;
   padding: 1rem;
 `;
 
 const ProjectFrameworks = styled.div`
-  display: block;
+  position: left;
   margin-top: 1rem;
   font-size: 0.9rem;
   color: #666;
@@ -79,20 +84,22 @@ export default function Home() {
           <ProjectTitle>plant-World</ProjectTitle>
           <ContentWrapper>
             <Link href="https://plant-world-flame.vercel.app/">
-              <StyledImage
-                src={plantWorldPicture.src}
-                alt="Beispielbild von Pflanzen in Wohnung"
-              />
+              <StyledImage src={plantWorldPicture.src} alt="Zimmerpflanzen" />
             </Link>
-            <ProjectDescription>
-              Die App ist der ideale Begleiter für neue und erfahrene
-              Pflanzenliebhaber. Sie bietet Pflegetipps für viele Pflanzen in
-              deiner Sammlung. Benutzer können ihre eigenen Pflanzen hinzufügen.
-              Eine intuitive Benutzeroberfläche macht es einfach, den Überblick
-              über die Pflanzenpflege zu behalten. Mit dieser App wird dein
-              Zuhause zum grünen Paradies! Die App ist noch im Aufbau,
-              schließlich gibt es immer etwas zu verbessern.
-            </ProjectDescription>
+            <DescriptionContainer>
+              <ProjectDescription>
+                Die App ist der ideale Begleiter für neue und erfahrene
+                Pflanzenliebhaber. Sie bietet Pflegetipps für viele Pflanzen in
+                deiner Sammlung. Benutzer können ihre eigenen Pflanzen
+                hinzufügen. Eine intuitive Benutzeroberfläche macht es einfach,
+                den Überblick über die Pflanzenpflege zu behalten. Mit dieser
+                App wird dein Zuhause zum grünen Paradies!
+              </ProjectDescription>
+              <ProjectDescription>
+                Die App ist noch im Aufbau, schließlich gibt es immer etwas zu
+                verbessern.
+              </ProjectDescription>
+            </DescriptionContainer>
           </ContentWrapper>
           <ProjectFrameworks>
             React / Next.js / Styled Components
@@ -107,13 +114,19 @@ export default function Home() {
                 src="https://beispiel.com/"
                 alt="Beispielbild der Finanz App"
               />
-            </Link>
-            <ProjectDescription>
-              Entdecke unsere intuitive Finanz-App für eine mühelose Verfolgung
-              deiner Einnahmen und Ausgaben. Setze dir Sparziele und beobachte,
-              wie du sie erreichst! Ideal auch für junge Menschen, um ihre
-              Finanzplanung zu beginnen und ein solides Fundament aufzubauen.
-            </ProjectDescription>
+            </Link>{" "}
+            <DescriptionContainer>
+              <ProjectDescription>
+                Entdecke unsere intuitive Finanz-App für eine mühelose
+                Verfolgung deiner Einnahmen und Ausgaben. Setze dir Sparziele
+                und beobachte, wie du sie erreichst! Ideal auch für junge
+                Menschen, um ihre Finanzplanung zu beginnen und ein solides
+                Fundament aufzubauen.
+              </ProjectDescription>
+              <ProjectContainer>
+                Diese App ist das Abschlussprojekt von dem neue fische Bootcamp
+              </ProjectContainer>
+            </DescriptionContainer>
           </ContentWrapper>
           <ProjectFrameworks>
             React / Next.js / Styled Components / Node.js / MongoDB Atlas /
