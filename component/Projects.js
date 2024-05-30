fe7747409da3143c1f75f9f97156a6bc87e1e5e3
Nadine Grantz <nadine.grantz@googlemail.com>
2024-05-30 16:09:04 +0200

    delete first project style/arrangement

--- component/Projects.js
+++ component/Projects.js
@@ -4,69 +4,6 @@ import Link from "next/link";
 import plantWorldPicture from "../assets/plantWorldPicture.png";
 import budgetBuddy from "../assets/budgetBuddy.png";
 
-// export default function Home() {
-//   return (
-//     <SectionContainer id="projects">
-//       <StyledHeader>Projects</StyledHeader>
-//       <ProjectsWrapper>
-//         <ProjectContainer>
-//           <ProjectTitle>plant-World</ProjectTitle>
-//           <ContentWrapper>
-//             <Link href="https://plant-world-flame.vercel.app/">
-//               <StyledImage src={plantWorldPicture.src} alt="Zimmerpflanzen" />
-//             </Link>
-//             <DescriptionContainer>
-//               <ProjectDescription>
-//                 Die App ist der ideale Begleiter für neue und erfahrene
-//                 Pflanzenliebhaber. Sie bietet Pflegetipps für viele Pflanzen in
-//                 deiner Sammlung. Benutzer können ihre eigenen Pflanzen
-//                 hinzufügen. Eine intuitive Benutzeroberfläche macht es einfach,
-//                 den Überblick über die Pflanzenpflege zu behalten. Mit dieser
-//                 App wird dein Zuhause zum grünen Paradies!
-//               </ProjectDescription>
-//               <ProjectDescription>
-//                 Die App ist noch im Aufbau, schließlich gibt es immer etwas zu
-//                 verbessern.
-//               </ProjectDescription>
-//             </DescriptionContainer>
-//           </ContentWrapper>
-//           <ProjectFrameworks>
-//             React / Next.js / Styled Components
-//           </ProjectFrameworks>
-//         </ProjectContainer>
-
-//         <ProjectContainer>
-//           <ProjectTitle>Budget Buddy</ProjectTitle>
-//           <ContentWrapper>
-//             <Link href="https://capstone-project-budget-buddy.vercel.app/">
-//               <StyledImage
-//                 src={budgetBuddy.src}
-//                 alt="Transaktionen in einer Finanzapp"
-//               />
-//             </Link>
-//             <DescriptionContainer>
-//               <ProjectDescription>
-//                 Entdecke unsere intuitive Finanz-App für eine mühelose
-//                 Verfolgung deiner Einnahmen und Ausgaben. Setze dir Sparziele
-//                 und beobachte, wie du sie erreichst! Ideal auch für junge
-//                 Menschen, um ihre Finanzplanung zu beginnen und ein solides
-//                 Fundament aufzubauen.
-//               </ProjectDescription>
-//               <ProjectContainer>
-//                 Diese App ist das Abschlussprojekt von dem neue fische Bootcamp
-//               </ProjectContainer>
-//             </DescriptionContainer>
-//           </ContentWrapper>
-//           <ProjectFrameworks>
-//             React / Next.js / Styled Components / Node.js / MongoDB Atlas /
-//             Mongoose / useSWR / Chart.js
-//           </ProjectFrameworks>
-//         </ProjectContainer>
-//       </ProjectsWrapper>
-//     </SectionContainer>
-//   );
-// }
-
 const projects = [
   {
     title: "plant-World",
@@ -123,6 +60,8 @@ const Image = styled.img`
 
 const Description = styled.p`
   margin-bottom: 10px;
+  position: absolut;
+  display-inline-block;
 `;
 
 const FrameworksContainer = styled.div`
