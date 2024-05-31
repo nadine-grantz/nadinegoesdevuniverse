8146ed49a98b4d664feb8d354c6e8f57b561ab29
Nadine Grantz <nadine.grantz@googlemail.com>
2024-05-31 14:17:14 +0200

    change img for budget buddy

--- component/Projects.js
+++ component/Projects.js
@@ -1,15 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 import Link from "next/link";
-import plantWorldPicture from "../assets/plantWorldPicture.png";
-import budgetBuddy from "../assets/budgetBuddy.png";
 import plantWorld from "../assets/plantWorld.png";
+import capstoneBudgetBuddy from "../assets/capstoneBudgetBuddy.png";
 
 const projects = [
   {
     title: "plantWorld",
     link: "https://plant-world-flame.vercel.app/",
-    image: { src: plantWorld.src, alt: "Zimmerpflanze" },
+    image: { src: plantWorld.src, alt: "Zimmerpflanzen" },
     description:
       " Die App ist der ideale Begleiter für neue und erfahrene Pflanzenliebhaber. Sie bietet Pflegetipps für viele Pflanzen in deiner Sammlung. Benutzer können ihre eigenen Pflanzen hinzufügen. Eine intuitive Benutzeroberfläche macht es einfach,den Überblick über die Pflanzenpflege zu behalten. Mit dieser App wird dein Zuhause zum grünen Paradies!",
     note: "Die plantWorld wird kontinuierlich verbessert, wenn du also wieder reinschaust kann sie schon wieder anders aussehen",
@@ -18,7 +17,10 @@ const projects = [
   {
     title: "Budget Buddy",
     link: "https://capstone-project-budget-buddy.vercel.app/",
-    image: { src: budgetBuddy.src, alt: "Transaktionen in einer Finanzapp" },
+    image: {
+      src: capstoneBudgetBuddy.src,
+      alt: "Transaktionen in einer Finanzapp",
+    },
     description:
       "Entdecke unsere intuitive Finanz-App für eine mühelose Verfolgung deiner Einnahmen und Ausgaben. Setze dir Sparziele und beobachte,wie du sie erreichst! Ideal auch für junge Menschen, um ihre Finanzplanung zu beginnen und ein solides Fundament aufzubauen.",
     note: "Diese App ist das Abschlussprojekt aus dem Bootcamp",
